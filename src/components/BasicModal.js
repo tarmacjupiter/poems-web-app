18908ad3495c2349bb4d80144d9a0de4fdd58b51
Anthony G Behery <behery6194@students.d211.org>
2022-03-13 00:59:07 -0600

    Modal styling

--- src/components/BasicModal.js
+++ src/components/BasicModal.js
@@ -11,12 +11,13 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "50%",
+  width: "25%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
-  p: 4,
-  backgroundColor: "#ececa3"
+  p: "2%",
+  backgroundColor: "#ececa3",
+  overflow: "hidden"
 };
 
 export default function BasicModal() {
